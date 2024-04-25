53c0b6a462615cff9e0aba3284b40e0142f49d56
Felipe Rosas <contato@eufelipe.com>
2024-04-25 16:37:57 -0300

    chore: wip

--- src/app/ar/page.tsx
+++ src/app/ar/page.tsx
@@ -8,56 +8,18 @@ const Ar = () => {
   const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
-    const loadInterval = setInterval(() => {
+    const checkAFrame = () => {
       if (
         typeof AFRAME !== "undefined" &&
-        AFRAME.components["zoom-camera"] === undefined
+        typeof AFRAME.registerComponent !== "undefined"
       ) {
-        registerZoomComponent();
-      } else if (
-        typeof AFRAME !== "undefined" &&
-        AFRAME.components["zoom-camera"] !== undefined
-      ) {
-        clearInterval(loadInterval);
         setIsClient(true);
+      } else {
+        setTimeout(checkAFrame, 100);
       }
-    }, 100);
-
-    const registerZoomComponent = () => {
-      AFRAME.registerComponent("zoom-camera", {
-        schema: {
-          zoomSpeed: { type: "number", default: 0.1 },
-          minDistance: { type: "number", default: 1 },
-          maxDistance: { type: "number", default: 10 },
-        },
-        init: function () {
-          this.handleWheel = (event: any) => {
-            event.preventDefault();
-            const delta = event.deltaY * this.data.zoomSpeed;
-            const direction = new AFRAME.THREE.Vector3();
-            this.el.object3D.getWorldDirection(direction);
-            direction.multiplyScalar(delta);
-            const newPosition = this.el.object3D.position
-              .clone()
-              .add(direction);
-            const distance = newPosition.length();
-
-            if (
-              distance > this.data.minDistance &&
-              distance < this.data.maxDistance
-            ) {
-              this.el.object3D.position.copy(newPosition);
-            }
-          };
-          this.el.addEventListener("wheel", this.handleWheel);
-        },
-        remove: function () {
-          this.el.removeEventListener("wheel", this.handleWheel);
-        },
-      });
     };
 
-    return () => clearInterval(loadInterval);
+    checkAFrame();
   }, []);
 
   return (
@@ -77,16 +39,12 @@ const Ar = () => {
         <a-scene embedded arjs="trackingMethod: best;">
           <a-marker preset="hiro">
             <a-entity
-              gltf-model="/Heart.glbf"
-              position="0 0.5 0"
+              gltf-model="/Heart.glb"
               scale="0.5 0.5 0.5"
+              position="0 0.5 0"
             ></a-entity>
           </a-marker>
-          <a-camera
-            position="0 1.6 2"
-            look-controls
-            zoom-camera="zoomSpeed: 0.5"
-          ></a-camera>
+          <a-camera position="0 1.6 2" look-controls></a-camera>
         </a-scene>
       )}
     </>
