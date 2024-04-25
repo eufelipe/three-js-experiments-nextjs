3a96d555370ae1e986d25568768ff187eceb9a0f
Felipe Rosas <contato@eufelipe.com>
2024-04-25 16:46:57 -0300

    chore: wip

--- src/app/ar/page.tsx
+++ src/app/ar/page.tsx
@@ -8,18 +8,30 @@ const Ar = () => {
   const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
-    const checkAFrame = () => {
-      if (
-        typeof AFRAME !== "undefined" &&
-        typeof AFRAME.registerComponent !== "undefined"
-      ) {
-        setIsClient(true);
-      } else {
-        setTimeout(checkAFrame, 100);
-      }
-    };
+    setIsClient(true);
 
-    checkAFrame();
+    if (typeof AFRAME !== "undefined") {
+      AFRAME.registerComponent("zoom-control", {
+        schema: {
+          speed: { type: "number", default: 0.1 }, // Controla a velocidade do zoom
+        },
+        init: function () {
+          this.handleWheel = (event: any) => {
+            event.preventDefault();
+            const delta = event.deltaY * -this.data.speed;
+            const camera = AFRAME.scenes[0].camera.el;
+            const direction = new AFRAME.THREE.Vector3();
+            camera.object3D.getWorldDirection(direction);
+            direction.multiplyScalar(delta);
+            camera.object3D.position.add(direction);
+          };
+          window.addEventListener("wheel", this.handleWheel);
+        },
+        remove: function () {
+          window.removeEventListener("wheel", this.handleWheel);
+        },
+      });
+    }
   }, []);
 
   return (
@@ -29,22 +41,33 @@ const Ar = () => {
       </Head>
       <Script
         src="https://aframe.io/releases/1.2.0/aframe.min.js"
+        onLoad={() => {
+          setIsClient(true);
+        }}
         strategy="beforeInteractive"
       />
       <Script
         src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"
         strategy="beforeInteractive"
       />
+
       {isClient && (
-        <a-scene embedded arjs="trackingMethod: best;">
-          <a-marker preset="hiro">
-            <a-entity
-              gltf-model="/Heart.glb"
-              scale="0.5 0.5 0.5"
-              position="0 0.5 0"
-            ></a-entity>
-          </a-marker>
-          <a-camera position="0 1.6 2" look-controls></a-camera>
+        <a-scene>
+          <a-light type="ambient" color="#555"></a-light>
+          <a-light type="point" intensity="1" position="1 2 3"></a-light>
+          <a-light type="point" intensity="1" position="-1 2 -3"></a-light>
+          <a-light type="point" intensity="1" position="0 3 0"></a-light>
+          <a-entity
+            gltf-model="/Heart.gltf"
+            position="0 0.5 -2"
+            scale="0.001 0.001 0.001"
+            zoom-control="speed:0.05"
+          ></a-entity>
+          <a-camera
+            position="0 1.6 0"
+            look-controls-enabled="true"
+            zoom-control
+          ></a-camera>
         </a-scene>
       )}
     </>
