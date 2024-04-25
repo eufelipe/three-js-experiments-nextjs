3cb6c891f6ba22e18af16255f8f27994432e0304
Felipe Rosas <contato@eufelipe.com>
2024-04-25 18:23:35 -0300

    chore: wip

--- src/app/ar/page.tsx
+++ src/app/ar/page.tsx
@@ -2,82 +2,10 @@
 
 import Head from "next/head";
 import Script from "next/script";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const Ar = () => {
   const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-
-    if (typeof AFRAME !== "undefined") {
-      AFRAME.registerComponent("zoom-control", {
-        schema: {
-          factor: { type: "number", default: 1 },
-        },
-        init: function () {
-          this.handleWheel = this.handleWheel.bind(this);
-          this.handleTouchMove = this.handleTouchMove.bind(this);
-          this.handleTouchStart = this.handleTouchStart.bind(this);
-          this.lastTouchDistance = null;
-
-          window.addEventListener("wheel", this.handleWheel);
-          window.addEventListener("touchmove", this.handleTouchMove);
-          window.addEventListener("touchstart", this.handleTouchStart);
-          window.addEventListener("touchend", () => {
-            this.lastTouchDistance = null;
-          });
-        },
-        remove: function () {
-          window.removeEventListener("wheel", this.handleWheel);
-          window.removeEventListener("touchmove", this.handleTouchMove);
-          window.removeEventListener("touchstart", this.handleTouchStart);
-          window.removeEventListener("touchend", () => {
-            this.lastTouchDistance = null;
-          });
-        },
-        handleWheel: function (event: any) {
-          event.preventDefault();
-          const scaleChange = event.deltaY * -0.005;
-          this.data.factor = Math.max(0.1, this.data.factor + scaleChange);
-          this.el.setAttribute(
-            "scale",
-            `${this.data.factor} ${this.data.factor} ${this.data.factor}`
-          );
-        },
-        handleTouchStart: function (event: any) {
-          if (event.touches.length === 2) {
-            const touch1 = event.touches[0];
-            const touch2 = event.touches[1];
-            this.lastTouchDistance = Math.hypot(
-              touch2.pageX - touch1.pageX,
-              touch2.pageY - touch1.pageY
-            );
-          }
-        },
-        handleTouchMove: function (event: any) {
-          if (event.touches.length === 2) {
-            const touch1 = event.touches[0];
-            const touch2 = event.touches[1];
-            const currentTouchDistance = Math.hypot(
-              touch2.pageX - touch1.pageX,
-              touch2.pageY - touch1.pageY
-            );
-            if (this.lastTouchDistance) {
-              const scaleChange =
-                (currentTouchDistance - this.lastTouchDistance) * 0.001;
-              this.data.factor = Math.max(0.1, this.data.factor + scaleChange);
-              this.el.setAttribute(
-                "scale",
-                `${this.data.factor} ${this.data.factor} ${this.data.factor}`
-              );
-            }
-            this.lastTouchDistance = currentTouchDistance;
-          }
-        },
-      });
-    }
-  }, []);
 
   return (
     <>
@@ -113,7 +41,7 @@ const Ar = () => {
             animation="property: rotation; to: -90 360 0; loop: true; dur: 100000"
             zoom-control
           ></a-entity>
-          <a-camera position="0 1.6 0" look-controls-enabled="true"></a-camera>
+          <a-camera look-controls-enabled="true"></a-camera>
         </a-scene>
       )}
     </>
