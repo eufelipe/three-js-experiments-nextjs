1f74ebf3a37fef0c67e7e38094f7bedf963b8d95
Felipe Rosas <contato@eufelipe.com>
2024-04-25 15:50:28 -0300

    chore: wip

--- src/app/ar/page.tsx
+++ src/app/ar/page.tsx
@@ -8,15 +8,30 @@ const Ar = () => {
   const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
-    if (
-      typeof AFRAME !== "undefined" &&
-      typeof AFRAME.registerComponent !== "undefined"
-    ) {
-      setIsClient(true);
-    } else {
-      setTimeout(() => {
-        setIsClient(true);
-      }, 1000);
+    setIsClient(true);
+
+    if (typeof AFRAME !== "undefined") {
+      AFRAME.registerComponent("zoom-control", {
+        schema: {
+          factor: { type: "number", default: 1 },
+        },
+        init: function () {
+          this.handleWheel = this.handleWheel.bind(this);
+          window.addEventListener("wheel", this.handleWheel);
+        },
+        remove: function () {
+          window.removeEventListener("wheel", this.handleWheel);
+        },
+        handleWheel: function (event: any) {
+          event.preventDefault();
+          const scaleChange = event.deltaY * -0.0005;
+          this.data.factor = Math.max(0.1, this.data.factor + scaleChange);
+          this.el.setAttribute(
+            "scale",
+            `${this.data.factor} ${this.data.factor} ${this.data.factor}`
+          );
+        },
+      });
     }
   }, []);
 
@@ -27,18 +42,29 @@ const Ar = () => {
       </Head>
       <Script
         src="https://aframe.io/releases/1.2.0/aframe.min.js"
+        onLoad={() => {
+          setIsClient(true);
+        }}
         strategy="beforeInteractive"
       />
       <Script
         src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"
         strategy="beforeInteractive"
       />
+
       {isClient && (
-        <a-scene embedded arjs="trackingMethod: best;">
-          <a-marker preset="hiro">
-            <a-box position="0 0.5 0" material="color: red;"></a-box>
-          </a-marker>
-          <a-entity camera></a-entity>
+        <a-scene>
+          <a-light type="ambient" color="#555"></a-light>
+          <a-light type="point" intensity="1" position="1 2 3"></a-light>
+          <a-light type="point" intensity="1" position="-1 2 -3"></a-light>
+          <a-light type="point" intensity="1" position="0 3 0"></a-light>
+          <a-entity
+            gltf-model="/Heart.gltf"
+            position="0 0.5 -2"
+            scale="0.001 0.001 0.001"
+            zoom-control
+          ></a-entity>
+          <a-camera position="0 1.6 0" look-controls-enabled="true"></a-camera>
         </a-scene>
       )}
     </>
